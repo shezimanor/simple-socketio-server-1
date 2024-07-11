e0e24aacb86d8dd699204d4f886f5345b9e83845
Ryan Chen <y.f.chenroderick@gmail.com>
2024-07-11 16:34:27 +0800

    feat: Add Socket.io

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Simple Express Server
+# SocketIO Express Server
+
+## 開發注意事項
+
+1. 改成 ES modules 的寫法。
+
+2. 目前 Server 和 Client 是整合狀態。
--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 import cors from 'cors';
 import express from 'express';
 import http from 'http';
+import { dirname } from 'path';
+import { Server } from 'socket.io';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const server = http.createServer(app);
+// io: Server Instance
+const io = new Server(server);
 const port = 3000;
+// ESModule 的 __dirname
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(cors());
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello Express Server</h1>');
+  response.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  // socket: Server Socket
+  console.log(`A new user connected with id: ${socket.id}`);
 });
 
 server.listen(port, () => {
