991d70f2ced71aa12dc9a62ecb3ac8408ba29328
Ryan Chen <y.f.chenroderick@gmail.com>
2024-07-12 16:46:02 +0800

    chore: Create sendUserInfo event

--- server.js
+++ server.js
@@ -9,18 +9,31 @@ const io = new Server(port, {
   }
 });
 
+const users = {};
+
 io.on('connection', (socket) => {
   // socket: Server Socket
   console.log(`A new user connected with id: ${socket.id}`);
   // 觸發事件
   socket.emit('message', 'Welcome My SocketIO universe!');
-  // 註冊事件
+
+  // 註冊事件: message
   socket.on('message', (msg) => {
     console.log(`伺服器收到來自[${socket.id}]的訊息：「${msg}」`);
     // 觸發事件（這兩個 message 互不相關）
     socket.emit('message', `我得到你的訊息了，訊息是「${msg}」`);
   });
+
+  // 註冊事件: greeting
   socket.on('greeting', (msg) => {
     console.log(`伺服器收到來自[${socket.id}]的 "greeting"：「${msg}」`);
+  });
+
+  // 註冊事件: send_user_info
+  socket.on('send_user_info', (userInfo) => {
+    console.log(`伺服器收到來自[${socket.id}]的 "send_user_info"：${userInfo}`);
+    // 加入新的 user
+    users[`${socket.id}`] = userInfo;
+    console.log('Now users: ', users);
   });
 });
