ea70319ce15097e2f08e96e08de32a7b475bdf86
Ryan Chen <y.f.chenroderick@gmail.com>
2024-07-12 15:50:21 +0800

    feat: Emit events

--- server.js
+++ server.js
@@ -12,8 +12,12 @@ const io = new Server(port, {
 io.on('connection', (socket) => {
   // socket: Server Socket
   console.log(`A new user connected with id: ${socket.id}`);
+  // 觸發事件
+  socket.emit('message', 'Welcome My SocketIO universe!');
   // 註冊事件
   socket.on('message', (msg) => {
     console.log(`伺服器收到來自[${socket.id}]的訊息：「${msg}」`);
+    // 觸發事件（這兩個 message 互不相關）
+    socket.emit('message', `我得到你的訊息了，訊息是「${msg}」`);
   });
 });
